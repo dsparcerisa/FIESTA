5d2f67bee2c32b58bf3558d67a5346195687e1af
GFNSouceControl <cmamsourcecontrol@gmail.com>
2020-06-26 13:22:39 +0200

    Separar config windows y mac

---
+++ FIESTAConfig_win.m
@@ -0,0 +1,6 @@
+Fcfg.logPath = 'C:\Users\CMAM\Documents\CMAM_logs';
+Fcfg.planPath = 'C:\Users\CMAM\Documents\CMAM_plans';
+Fcfg.filmPath = 'C:\Users\CMAM\Documents\CMAM_films';
+Fcfg.filmCalFile = 'C:\Users\CMAM\Documents\MATLAB\FIESTA\filmDosimetry\CoefFitCMAM_HG.mat';
+Fcfg.arduinoPort = 'COM3'; % for shutter
+Fcfg.velmexPort = 'COM4'; % for stage
