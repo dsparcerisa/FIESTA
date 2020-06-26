f38c3551916e30ea749aa515dee912562301aa88
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2020-06-26 10:02:12 +0200

    Add confidence interval to meanAndCenterMass

--- aux/meanAndCenterMass.m
+++ aux/meanAndCenterMass.m
@@ -1,4 +1,4 @@
-function [mask, xcentre, ycentre, sigmaX, sigmaY, meanvalue, maxX, maxY] = meanAndCenterMass(img,radius_pixels)
+function [mask, xcentre, ycentre, sigmaX, sigmaY, meanvalue, maxX, maxY, deltasigmaX, deltasigmaY, stdDose] = meanAndCenterMass(img,radius_pixels)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 Xvalues = 1:size(img, 2);
@@ -13,15 +13,20 @@ sumX = - (sumX - max(sumX));
 sumY = sum(img, 2);
 sumY = - (sumY - max(sumY));
 F1 = fit(Xvalues', sumX', 'gauss1');
+F1errors = confint(F1);
 xcentre = F1.b1;
 sigmaX = F1.c1 / sqrt(2);
+deltasigmaX = max(abs(F1errors(:,3)-F1.c1)) / sqrt(2);
 F2 = fit(Yvalues', sumY, 'gauss1');
+F2errors = confint(F2);
 ycentre = F2.b1;
 sigmaY = F2.c1 / sqrt(2);
+deltasigmaY = max(abs(F2errors(:,3)-F2.c1)) / sqrt(2);
 distance2 = (x-xcentre).^2 + (y-ycentre).^2;
 mask = distance2 <= (radius_pixels^2);
-img = - (img - max(img(:)));
+img = -img;
 meanvalue = mean2(img(mask==1));
+stdDose = std(img(mask==1));
 maxX = F1.a1;
 maxY = F2.a1;
 
