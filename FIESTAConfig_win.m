9c9b988437bf83857f6e01c368aa1db9f7a19a27
GFNSouceControl <cmamsourcecontrol@gmail.com>
2020-07-07 18:11:00 +0200

    COM5

--- FIESTAConfig_win.m
+++ FIESTAConfig_win.m
@@ -2,5 +2,5 @@ Fcfg.logPath = 'C:\Users\CMAM\Documents\CMAM_logs';
 Fcfg.planPath = 'C:\Users\CMAM\Documents\CMAM_plans';
 Fcfg.filmPath = 'C:\Users\CMAM\Documents\CMAM_films';
 Fcfg.filmCalFile = 'C:\Users\CMAM\Documents\MATLAB\FIESTA\filmDosimetry\CoefFitCMAM_HG.mat';
-Fcfg.arduinoPort = 'COM3'; % for shutter
+Fcfg.arduinoPort = 'COM5'; % for shutter
 Fcfg.velmexPort = 'COM4'; % for stage
