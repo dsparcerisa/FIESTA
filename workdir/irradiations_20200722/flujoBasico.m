3c08f090b8a57ef4ae314faa91b98fbe898652a7
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2020-07-22 08:43:43 +0200

    Agregar cálculo de E

--- workdir/irradiations_20200722/flujoBasico.m
+++ workdir/irradiations_20200722/flujoBasico.m
@@ -9,3 +9,12 @@ t_open = 1; % segundos
 t_wait = 3; % segundos
 
 irradiatePlanOnlyShutter(Fcfg, COMshutter, numCiclos, t_open, t_wait)
+
+%% Cálculo de la energía
+separacionAire_cm = 8.1;
+E0 = 8.22;
+
+dz_cm = 0.001;
+Zval = 0:dz_cm:separacionAire_cm;
+energyA = energyStoppingPower(E0, Zval);
+finalE = energyA(end)
