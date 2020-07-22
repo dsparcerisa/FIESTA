6be52eaab6bff4e959db9aa97c981e56e62b4062
GFNSouceControl <cmamsourcecontrol@gmail.com>
2020-07-22 09:14:40 +0200

    agragegada irradiacion manual

--- workdir/irradiations_20200722/flujoBasico.m
+++ workdir/irradiations_20200722/flujoBasico.m
@@ -4,11 +4,16 @@ FIESTAConfig_win
 startShutter
 
 %% Realizar irradiacion
-numCiclos = 3;
-t_open = 1; % segundos
+numCiclos = 1;
+t_open = 10; % segundos
 t_wait = 3; % segundos
 
 irradiatePlanOnlyShutter(Fcfg, COMshutter, numCiclos, t_open, t_wait)
+
+%% Abrir shutter en manual (NO LOGGEA)
+t_manual = 0.5;
+configureShutter(COMshutter, 't', t_manual);
+shutter(COMshutter, 'n', 1);
 
 %% Cálculo de la energía
 separacionAire_cm = 8.1;
