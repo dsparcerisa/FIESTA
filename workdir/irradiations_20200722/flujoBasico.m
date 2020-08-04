906027f1ab74df8fded2f72fd9d59a0d5b7a4169
GFNSouceControl <cmamsourcecontrol@gmail.com>
2020-08-04 15:08:46 +0200

    Irradiaciones con shutter 22-jul

--- workdir/irradiations_20200722/flujoBasico.m
+++ workdir/irradiations_20200722/flujoBasico.m
@@ -5,19 +5,19 @@ startShutter
 
 %% Realizar irradiacion
 numCiclos = 1;
-t_open = 10; % segundos
-t_wait = 3; % segundos
+t_open = 0.03 % segundos
+t_wait = 30; % segundos
 
 irradiatePlanOnlyShutter(Fcfg, COMshutter, numCiclos, t_open, t_wait)
 
 %% Abrir shutter en manual (NO LOGGEA)
-t_manual = 0.5;
+t_manual = 200;
 configureShutter(COMshutter, 't', t_manual);
 shutter(COMshutter, 'n', 1);
 
 %% Cálculo de la energía
-separacionAire_cm = 8.1;
-E0 = 8.22;
+separacionAire_cm = 9.2;
+E0 = 8.755;
 
 dz_cm = 0.001;
 Zval = 0:dz_cm:separacionAire_cm;
