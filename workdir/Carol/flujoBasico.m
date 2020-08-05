7f0267848b6dd277ab6ae81906ef36bb82c7a4cf
GFNSouceControl <cmamsourcecontrol@gmail.com>
2020-08-05 17:16:56 +0200

    cambios en el flujo básico

--- workdir/Carol/flujoBasico.m
+++ workdir/Carol/flujoBasico.m
@@ -8,12 +8,16 @@ startStage
 stageControlStart(COMstage)
 
 %% Leer plan
-planName = 'pruebaErrores.txt'; % Cambiar aqui
+planName = 'planPrueba.txt'; % Cambiar aqui
 planPath = [Fcfg.planPath filesep planName];
 thePlan = readPlan(planPath);
 
 %% Irradiar plan
-maxIterations = 1; % NUMERO DE REPETICIONES
+maxIterations = 3; % NUMERO DE REPETICIONES (SOLO SE CAMBIA AQUI, NO ESTA EN EL PLAN)
 vector2startPoint = [0 0 0]; % para mover todo el plan un vector constante
 stageLimits = [-11 11 -11 11 -11 11]; % corregir antes de irradiar para evitar fallos de movimiento
 irradiatePlanOnlyStage(Fcfg, COMstage, thePlan, vector2startPoint, stageLimits, maxIterations)
+
+%% Irradiar plan (con shutter)
+startShutter
+irradiatePlan(COMstage, COMshutter, thePlan, vector2startPoint, stageLimits, 0.0)
