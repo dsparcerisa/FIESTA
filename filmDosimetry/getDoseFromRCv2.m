2bc02b6635ca9c3c41edf8b751b859e6ff097d77
Daniel Sánchez Parcerisa <dsparcerisa@ucm.es>
2021-02-15 12:41:55 +0100

    Film dosimetry tools

--- filmDosimetry/getDoseFromRCv2.m
+++ filmDosimetry/getDoseFromRCv2.m
@@ -1,13 +1,12 @@
-function [dose, varMat] = getDoseFromRC(I, CoefR, CoefG, CoefB, pixelsXCM, deltas)
+function [dose, varMat] = getDoseFromRCv2(I, CoefR, CoefG, pixelsXCM, deltas)
 
 pxmax = double(intmax(class(I))); % maximum pixel value 2^16
 
 IODR = log10(pxmax./double(I(:,:,1)));
 IODG = log10(pxmax./double(I(:,:,2)));
-IODB = log10(pxmax./double(I(:,:,3)));
 
 if ~exist('deltas')
-    delta = -0.3:0.001:0.3;
+    delta = 0.8:0.002:1.2;
 else
     delta = deltas;
 end
@@ -21,9 +20,8 @@ for k = 1:numel(delta)
         for j = 1:size(I,2)
             DR = (CoefR(3)*10^(-IODR(i,j)*delta(k))-CoefR(1))/(CoefR(2)-10^(-IODR(i,j)*delta(k)));
             DG = (CoefG(3)*10^(-IODG(i,j)*delta(k))-CoefG(1))/(CoefG(2)-10^(-IODG(i,j)*delta(k)));
-            DB = (CoefB(3)*10^(-IODB(i,j)*delta(k))-CoefB(1))/(CoefB(2)-10^(-IODB(i,j)*delta(k)));
             
-            dev = (DR-DG)^2+(DR-DB)^2+(DB-DG)^2;
+            dev = (DR-DG)^2;
             
             if dev < dev_min(i,j)
                 dev_min(i,j) = dev;
@@ -35,9 +33,8 @@ end
 
 DR = (CoefR(3)*10.^(-IODR.*delta0)-CoefR(1))./(CoefR(2)-10.^(-IODR.*delta0));
 DG = (CoefG(3)*10.^(-IODG.*delta0)-CoefG(1))./(CoefG(2)-10.^(-IODG.*delta0));
-DB = (CoefB(3)*10.^(-IODB.*delta0)-CoefB(1))./(CoefB(2)-10.^(-IODB.*delta0));
 
-doseRGB = (DR+DG+DB)./3;
+doseRGB = (DR+DG)./2;
 
 %figure(1); imagesc(doseRGB); title('Dose map (RGB)'); caxis([0 10]);
 %figure(2); imagesc(delta0); title('Delta0'); caxis([0.8 1.2]);
