af6ea56848de541c72a38ea7df5d74a0fe12f163
GFNSouceControl <cmamsourcecontrol@gmail.com>
2021-02-22 16:42:48 +0100

    Pruebas y bugfix irradiate with Pause

--- workdir/commissioning_imp_23022020/irradiarPlanPausas.m
+++ workdir/commissioning_imp_23022020/irradiarPlanPausas.m
@@ -19,8 +19,8 @@ planPath = [Fcfg.planPath filesep planName];
 thePlan = readPlan(planPath);
 
 %% Irradiar plan
-maxIterations = 50; % NUMERO DE REPETICIONES (SOLO SE CAMBIA AQUI, NO ESTA EN EL PLAN)
+maxIterations = 1; % NUMERO DE REPETICIONES (SOLO SE CAMBIA AQUI, NO ESTA EN EL PLAN)
 vector2startPoint = [0 0 0]; % para mover todo el plan un vector constante
-stageLimits = [-11 11 -11 11 -11 11]; % corregir antes de irradiar para evitar fallos de movimiento
+stageLimits = [-5 5 -5 5 -5 0]; % corregir antes de irradiar para evitar fallos de movimiento
 irradiatePlanOnlyStage(Fcfg, COMstage, thePlan, vector2startPoint, stageLimits, maxIterations)
 
